21b0336bf24a422e5cba146b953fe075dba612f4
akhireddi <akhilagreddy.22@gmail.com>
2024-10-03 12:13:29 +0530

    datatypes in java examples

---
+++ hey.java
@@ -0,0 +1,25 @@
+public class hey{
+	
+	public static void main(String[]args){
+	    byte[]DaysInMonth={31,30,31,30,28,29,30,31};
+	    short[]AttendancePercentage={25,35,40,85,95};
+	    int[]marksInSocSub={12,13,50,45,69,76,78,79,88,98};
+	    long[]phoneNum={2345l,6788l,3547l,2345l,3456l,3416l,7890l,8765l,4565l,8796l};
+	    float[]weight={35.5f,45.3f,20.9f,30.4f,4.0f,35.6f,75.8f,85.5f,65.5f,55.0f};
+	    double[]decimals={2.09d,3.0d,3.00d,8.25d,8.26d,8.27d,9.43d,9.87d,9.876d,1.05d};
+	    boolean[]isItDrizzling={true,false,false,true};
+	    char[]gender={'f','m','t'};
+
+	        System.out.println(DaysInMonth[3]);
+	        System.out.println(AttendancePercentage[3]);
+	        System.out.println(marksInSocSub[4]);
+	        System.out.println(phoneNum[5]);
+	        System.out.println(weight[3]);
+	        System.out.println(decimals[8]);
+	        System.out.println(isItDrizzling[2]);
+	        System.out.println(gender[2]);
+
+	}
+
+	
+}
